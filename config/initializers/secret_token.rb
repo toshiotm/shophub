fb874defea18735c254724000ada968c87b488bb
toshiotm <toshi.otm@gmail.com>
2013-04-10 12:23:02 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shophub::Application.config.secret_key_base = 'f8ee39a8eb1d3431047421b971550068942b327cd88a86f5339fb2ddedf2f9b75c2250ef6c66660efe10a43a9b8db39edf4466247fc8d5fa338fdb8f8eeb93c5'
